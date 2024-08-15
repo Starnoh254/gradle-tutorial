621391a0652cbe1a654fdee2f561a1972c1f20b7
Starnoh John <130779431+Starnoh254@users.noreply.github.com>
2024-08-15 18:09:38 +0300

    Initial Commit

---
+++ src/test/java/com/stano/GradleTutorialTest.java
@@ -0,0 +1,11 @@
+package com.stano;
+
+import org.junit.Test;
+
+public class GradleTutorialTest {
+	
+	@Test
+	public void verifyNoExceptionThrown() {
+		GradleTutorial.main(new String[]{});
+	}
+}
